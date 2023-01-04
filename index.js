00aac39ded617e85683515e21c88306c4901911f
Tyrabims <abimbola.anibaba@yahoo.com>
2023-01-05 00:53:25 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const firstContainer= document.getElementById("#container");
+ 
+const thankYouContainer = document.querySelector(".thankyou-container")
+
+const btn = document.querySelectorAll("btn");
+ 
+const submitButton = document.getElementsById("#submitt");
+
+const backButton = document.getElementsById("#backBtn");
+// function btnSelected() {
+ 
+// }
+
+//select number 
+// then click on submit button
+//element.addEventListener("click", function)
+
+
+// submitButton.addEventListener("click", () => {
+//   thankYouContainer.classList.remove(".thankyou-container")
+// }) 
+
